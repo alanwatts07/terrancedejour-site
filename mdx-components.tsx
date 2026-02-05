121da60c252219897201628dfe775349ae35c982
alanwatts07 <morpheus@alanbox.localdomain>
2026-02-05 09:55:02 -0500

    Remove mdx-components.tsx - not needed for Nextra 4.x

--- mdx-components.tsx
+++
@@ -1,9 +0,0 @@
-import type { MDXComponents } from 'mdx/types'
-import defaultComponents from 'nextra-theme-docs/mdx-components'
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...defaultComponents,
-    ...components,
-  }
-}
